dcdfbedbf94c3341ee814e4144758290852bd105
Jason Crescenzo <crescenzojay@gmail.com>
2018-10-15 22:03:28 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,3 +19,12 @@ def first_challenge
   contacts
 end
 
+def remove_strawberry(contacts)
+  contacts.each do |name, data|
+    data.each do |attribute, value|
+      if attribute == :favorite_ice_cream_flavors
+        data[:favorite_ice_cream_flavors].delete_if { |i| i == "strawberry" }
+      end
+    end
+  end
+end
